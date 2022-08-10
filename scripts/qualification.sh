87e33771ca3d41d48fc46dade35e9f8fcc369c80
XuhuiZhou <zhouxuhui2018@gmail.com>
2022-08-10 19:25:56 -0400

    add qulification running scripts

---
+++ scripts/qualification.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python ./tools/qualification.py \
+    --qual_score_files "./data/mturk/qual1_results.csv" \
+    --qual_id "35ZCRXGG90HGCWSJOCDTWO3DQC8GS0" \
+    --qual_score_col "WorkerScores" \
+
