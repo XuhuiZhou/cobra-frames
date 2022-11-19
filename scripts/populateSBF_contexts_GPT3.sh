3b8cd4892dda719fc0b182f3f4821cad9df93367
XuhuiZhou <zhouxuhui2018@gmail.com>
2022-11-19 14:21:59 -0500

    🐛 fix the populate context bug

--- scripts/populateSBF_contexts_GPT3.sh
+++ scripts/populateSBF_contexts_GPT3.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
 
-EXAMPLE='./data/examples.v2.contextOnlyDiverse.csv'
-#EXAMPLE_CONT='./data/examples.v2.contextOnlyCombined.csv'
-#### pilot one
-#INPUT='./data/sbic.trn.r60.gpt3socCont.csv'
-#OUTPUT='./data/sbic.inference.csv'
-#INPUT='./data/mAgr.r60.gpt3socCont.csv'
-#OUTPUT='./data/mAgr.inference.csv'
+EXAMPLE='./data/prompts/examples.v2.contextOnlyDiverse.csv'
 
-#### pilot two
-# INPUT='./data/source_data/mAgr.onlyQuotes.csv'
-# OUTPUT='./data/inference_data/mAgr.inference2_ex.csv'
-# INPUT='./data/source_data/SBIC.v2.agg.trn.csv'
-# OUTPUT='./data/inference_data/sbic.inference_ex.csv'
-for i in {91..200}
+for i in {201..202}
 do
     echo "Populate the $i toxigen file"
     INPUT="./data/cleaned_data/toxigen_shuffled/toxigen_${i}.csv"
     OUTPUT="./data/inference_data/toxigen_shuffled/toxigen_${i}.csv"
     SEED=$i
 
-    python ./gpt3_generation/populateSBFwithGPT3.v5.py \
+    python ./gpt3_generation/populateContextswithGPT3.py \
         --input_file $INPUT \
         --example_file_context $EXAMPLE \
         --output_file $OUTPUT \
