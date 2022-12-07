d5786a07b1064a4ec267fba6d11fb9b42dc1516e
XuhuiZhou <zhouxuhui2018@gmail.com>
2022-12-07 11:10:49 -0500

    ✨ split explanation data script

---
+++ tools/sbf_modeling/split_explanation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+folder = "data/inference_data/toxigen_explanations"
+
+df = pd.read_csv(f"{folder}/toxigen_explanations.csv")
+df = df.rename(columns={"speechContext": "situationalContext"})
+
+df_train = df[:12612]
+df_val = df[12612:]
+
+df_train.to_csv(f"{folder}/toxigen_explanations_train.csv", index=False)
+df_val.to_csv(f"{folder}/toxigen_explanations_val.csv", index=False)
