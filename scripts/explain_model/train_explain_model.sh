da41317c5a941636b066530f423e0f14080dbdb3
Hao Zhu <prokilchu@gmail.com>
2023-01-02 11:56:00 -0500

    fix the bash model parameter bug

--- scripts/explain_model/train_explain_model.sh
+++ scripts/explain_model/train_explain_model.sh
@@ -11,9 +11,9 @@ if [ -z " $EXP " ] ; then
     exit 1
 fi
 
-if [[ $EXP=="train_xxl" ]]; then
+if [[ $EXP == "train_xxl" ]]; then
     python sbf_modeling/train.py --gin_file="scripts/explain_model/explain_model.gin" --gin_file="exp/configs/train_explain_model_xxl_2_epoch.gin" --gin.MODEL_DIR="'.log/explain-model-xxl'" --gin.MODE="'deployment'"
-elif [[ $EXP=="train_xl" ]]; then
+elif [[ $EXP == "train_xl" ]]; then
     python sbf_modeling/train.py --gin_file="scripts/explain_model/explain_model.gin" --gin_file="exp/configs/train_explain_model_xl_2_epoch.gin" --gin.MODEL_DIR="'.log/explain-model-xl'" --gin.MODE="'deployment'"
 else
     echo "Experiment name not found"
