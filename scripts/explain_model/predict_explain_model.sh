805875a6226f5d38aa0566661b2085882b746eac
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-01-02 17:20:19 -0500

    save prediction files to the

--- scripts/explain_model/predict_explain_model.sh
+++ scripts/explain_model/predict_explain_model.sh
@@ -3,4 +3,8 @@
 # chmod +x scripts/explain_model/train_explain_model.sh
 # ./scripts/explain_model/train_explain_model.sh
 
-python sbf_modeling/inference.py --gin_file="scripts/explain_model/explain_model_inference.gin" --gin.MODEL_DIR="'.log/explain-model'" --gin.MODE="'deployment'" --gin.BATCH_SIZE=32
+python sbf_modeling/inference.py \
+    --gin_file="scripts/explain_model/explain_model_inference.gin" \
+    --gin.MODEL_DIR="'.log/explain-model'" \
+    --gin.MODE="'deployment'" \
+    --gin.BATCH_SIZE=32
