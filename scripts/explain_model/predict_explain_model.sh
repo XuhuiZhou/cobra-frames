f9de35d77dee60ad572d657097855a156ecf354b
Hao Zhu <prokilchu@gmail.com>
2023-01-04 15:15:19 -0500

    t5 predict choices

--- scripts/explain_model/predict_explain_model.sh
+++ scripts/explain_model/predict_explain_model.sh
@@ -2,10 +2,34 @@
 # This script should be excuted in the root directory of the project
 # chmod +x scripts/explain_model/train_explain_model.sh
 # ./scripts/explain_model/train_explain_model.sh
-MODEL_DIR='.log/explain-model-xl-w-o-context'
+# MODEL_DIR='.log/explain-model-xl-w-o-context'
+EXP="$1"
 
-python sbf_modeling/inference.py \
+if [ -z " $EXP " ] ; then
+    echo "Please specify the experiment name"
+    exit 1
+fi
+
+if [[ $EXP == "xxl" ]]; then
+    python sbf_modeling/inference.py \
+        --gin_file="scripts/explain_model/explain_model_inference.gin" \
+        --gin.MODEL_DIR="'.log/explain-model-xxl'" \
+        --gin.MODE="'deployment'" \
+        --gin.BATCH_SIZE=4
+elif [[ $EXP == "xl_w_o_context" ]]; then
+    python sbf_modeling/inference.py \
     --gin_file="scripts/explain_model/explain_model_inference.gin" \
-    --gin.MODEL_DIR="$MODEL_DIR" \
+    --gin_file="exp/configs/without_context.gin" \
+    --gin.MODEL_DIR="'.log/explain-model-xl-w-o-cotext'" \
     --gin.MODE="'deployment'" \
     --gin.BATCH_SIZE=4
+elif [[ $EXP == "xl" ]]; then
+    python sbf_modeling/inference.py \
+    --gin_file="scripts/explain_model/explain_model_inference.gin" \
+    --gin.MODEL_DIR="'.log/explain-model-xl'" \
+    --gin.MODE="'deployment'" \
+    --gin.BATCH_SIZE=4
+else
+    echo "Experiment name not found"
+    exit 1
+fi
