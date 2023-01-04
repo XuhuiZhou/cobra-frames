826b43016d66a730aa9cff755839c0c8f59f081b
Hao Zhu <prokilchu@gmail.com>
2023-01-04 10:31:59 -0500

    predict script

--- scripts/explain_model/predict_explain_model.sh
+++ scripts/explain_model/predict_explain_model.sh
@@ -2,9 +2,10 @@
 # This script should be excuted in the root directory of the project
 # chmod +x scripts/explain_model/train_explain_model.sh
 # ./scripts/explain_model/train_explain_model.sh
+MODEL_DIR='.log/explain-model-xl-w-o-context'
 
 python sbf_modeling/inference.py \
     --gin_file="scripts/explain_model/explain_model_inference.gin" \
-    --gin.MODEL_DIR="'.log/explain-model-xl'" \
+    --gin.MODEL_DIR="$MODEL_DIR" \
     --gin.MODE="'deployment'" \
     --gin.BATCH_SIZE=4
