60a367da0b629d6330e60ee5dbb8a20b928fd16c
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-01-07 14:06:40 -0500

    ✨ final version for generating adversarial contexts

--- scripts/populateSBF_AdvContexts_GPT3.sh
+++ scripts/populateSBF_AdvContexts_GPT3.sh
@@ -10,11 +10,16 @@ EXAMPLE='./data/prompts/examples.v2.context_statementAdv.csv'
 # OUTPUT='./data/inference_data/sbic.inference_ex.csv'
 
 echo "Generate the adversarial contexts and related statements"
+INPUT="./data/source_data/mAgr.onlyQuotes.csv"
+OUTPUT_FILE="mAgr_contexts.csv"
 OUTPUT_FOLDER="./data/inference_data/adversarial_contexts_statements/"
 
+# Given the input file, dataset_size equals to 0 means that we will use all the examples in the input file
 python ./gpt3_generation/populateAdvContextswithGPT3.py \
     --example_file $EXAMPLE \
     --output_dir $OUTPUT_FOLDER \
     --post 'This is the placeholder' \
-    --dataset_size 20 \
-    --n_examples 5
+    --dataset_size 0 \
+    --n_examples 5 \
+    --output_file $OUTPUT_FILE \
+    --input_file $INPUT
