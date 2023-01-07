d299779adf7519f13bfbd7f8979905771e73087c
Hao Zhu <prokilchu@gmail.com>
2023-01-06 19:41:23 -0500

    💚 remove bertscore from test

--- tests/sbf_modeling_code/test_evaluation_utils.py
+++ tests/sbf_modeling_code/test_evaluation_utils.py
@@ -2,11 +2,10 @@ from sbf_modeling.evaluation_utils import generic_evaluate_function
 
 
 def test_generic_evaluate_function():
-    metric_names = ["bleu", "rouge", "meteor", "bertscore"]
+    metric_names = ["bleu", "rouge", "meteor"]
     predictions = ["this is a test", "this is a test"]
     references = ["this is a test", "this is a test"]
     results = generic_evaluate_function(metric_names, predictions, references)
     assert results["bleu"] == 1.0
     assert results["rouge"] == 1.0
     assert results["meteor"] > 0.9
-    assert results["bertscore"] == 1.0
