8d899b74253faf8fb1103d3c21b16ef35beda554
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-01-10 15:38:10 -0500

    ✨ train small model

--- scripts/explain_model/train_explain_model.sh
+++ scripts/explain_model/train_explain_model.sh
@@ -19,6 +19,8 @@ elif [[ $EXP == "train_xl" ]]; then
     python sbf_modeling/train.py --gin_file="scripts/explain_model/explain_model.gin" --gin_file="exp/configs/train_explain_model_xl_2_epoch.gin" --gin.MODEL_DIR="'.log/explain-model-xl'" --gin.MODE="'deployment'"
 elif [[ $EXP == "train_large" ]]; then
     python sbf_modeling/train.py --gin_file="scripts/explain_model/explain_model.gin" --gin_file="exp/configs/train_explain_model_large_2_epoch.gin" --gin.MODEL_DIR="'.log/explain-model-large'" --gin.MODE="'deployment'"
+elif [[ $EXP == "train_small" ]]; then
+    python sbf_modeling/train.py --gin_file="scripts/explain_model/explain_model.gin" --gin_file="exp/configs/train_explain_model_small_2_epoch.gin" --gin.MODEL_DIR="'.log/explain-model-small'" --gin.MODE="'deployment'"
 else
     echo "Experiment name not found"
     exit 1
